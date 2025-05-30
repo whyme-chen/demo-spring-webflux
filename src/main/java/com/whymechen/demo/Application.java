8bf2638b38900a91a4925107ce740b91d41799b9
chenwenjian <chenwenjian@axzo.cn>
2025-05-30 14:48:15 +0800

    feat(webflux): 基本架构搭建

---
+++ src/main/java/com/whymechen/demo/Application.java
@@ -0,0 +1,15 @@
+package com.whymechen.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication(scanBasePackages = "com.whymechen.demo")
+public class Application {
+    public static void main( String[] args ) {
+        SpringApplication.run(Application.class, args);
+    }
+}
